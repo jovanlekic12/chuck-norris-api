71dedfc6febc422bb6dee0e2ad671d3b3faf66cf
jovanlekic12 <lekicjovan1212@gmail.com>
2024-08-25 13:41:34 +0200

    finish project

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
-import './style.css'
+"use strict";
+import "./style.css";
+const btn = document.querySelector(".load__joke__btn");
+const loadJoke = async () => {
+  try {
+    const chuckNorrisFetch = await fetch(
+      "https://api.chucknorris.io/jokes/random",
+      {
+        headers: {
+          Accept: "application/json",
+        },
+      }
+    );
+
+    const jokeData = await chuckNorrisFetch.json();
+    document.querySelector(".loading__joke").innerHTML = jokeData.value;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+btn.addEventListener("click", loadJoke);
